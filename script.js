ca9cc85ecdf50c72f59343990f2eef632a9c0bd6
kamilakupinski <kupinski.kamila@escola.pr.gov.br>
2026-07-27 12:11:41 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-function insert (num){
-    var numero = document.getElementById ('resultado').innerHTML;
-    document.getElementById ('resultado').innerHTML=numero+num;
+function insert(num) {
+    var numero=document.getElementById('resultado').innerHTML;
+    document.getElementById('resultado').innerHTML=numero+num;
 }
 function clean(){
-    document.getElementById ('resultado').innerHTML="";
+document.getElementById('resultado').innerHTML="";
 }
 function back(){
-    var resultado = document.getElementById ('resultado').innerHTML;
-document.getElementById('resultado').innerHTML=resultado.substring(0,rsultado.length-1);
-}
-function calcular ()
-    var reultao = document.getElementById ('resultao').innerHTML;
-    if resultado{
-        document.getElementById('resultado').innerHTML = eval(resultado);
+     var reultado=document.getElementById('resultado').innerHTML;
+     document.getElementById('resultado').innerHTML=resultado.substeing(0,resultado.length-1);
     }
-    else { document.getElementById ('resultado').innerHTML = "nada para calcular"
+function calcular(){
+    var resultado= document.getElementById('resultado').innerHTML;
+if(reultado){
+    document.getElementById('resultado').innerHTML=eval(resultado);
+}
+else{
+    document.getElementById('resultado').innerHTML="nada para calcular"
+}
 }
